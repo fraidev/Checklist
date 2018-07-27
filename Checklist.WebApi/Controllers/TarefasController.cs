ee812e8cf0d1055fc496178ef91f08481e0b1ebf
Felipe.Cardozo <felipe.cardozo@sapiensia.com>
2018-07-27 15:01:53 -0300

    Gets de Controlles funcionam

---
+++ Checklist.WebApi/Controllers/TarefasController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Checklist.WebApi.Domain;
+using Checklist.WebApi.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+
+namespace Checklist.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : Controller
+    {
+        // GET api/tarefas
+        [HttpGet]
+        public ActionResult<IEnumerable<Tarefa>> Get()
+        {
+            List<Tarefa> tarefas;
+
+            var sessionFactory =  NHibernateHelper.CreateSessionFactory();
+            using (ISession session = sessionFactory.OpenSession())  // Open a session to conect to the database
+            {
+                tarefas = session.Query<Tarefa>().ToList(); //  Querying to get all the books
+            }
+
+            return tarefas;
+        }
+    }
+}
---
+++ Checklist.WebApi/Controllers/UsuariosController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Checklist.WebApi.Domain;
+using Checklist.WebApi.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+
+namespace Checklist.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : Controller
+    {
+        // GET api/usuarios
+        [HttpGet]
+        public ActionResult<IEnumerable<Usuario>> Get()
+        {
+            List<Usuario> usuarios;
+
+            var sessionFactory =  NHibernateHelper.CreateSessionFactory();
+            using (ISession session = sessionFactory.OpenSession())  // Open a session to conect to the database
+            {
+                usuarios = session.Query<Usuario>().ToList(); //  Querying to get all the books
+            }
+
+            return usuarios;
+        }
+    }
+}
